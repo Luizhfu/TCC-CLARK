75062f7a5a42ddf206987b4c5c41875762c3dec0
Luizhfu <se.luiz.eduardo@gmail.com>
2025-10-20 15:55:05 -0300

    atualização-O-Recomeço

--- src/pages/inicio.jsx
+++ src/pages/inicio.jsx
@@ -4,31 +4,28 @@ export default function Inicio() {
 
 
   return (
-<div className='container-inicio'>
-
-      <img className='logo' src="/src/assets/images/Captura_de_tela_2025-10-14_214454-removebg-preview.png" alt="" />
-
 <div className='Clark'>
 
 <div className='entrada'>
-              <img className='logo2' src="/src/assets/images/Captura_de_tela_2025-10-14_214454-removebg-preview.png" alt="" />
+<img className='logo2' src="/src/assets/images/Captura_de_tela_2025-10-14_214454-removebg-preview.png" alt="" />
 
-              <a className='links' href="/src/pages/entrar.jsx">Entrar</a>
-              <a className='links' href="/src/pages/registrar.jsx">Resgistrar</a>
+<a className='links' href="/src/pages/entrar.jsx">Entrar</a>
+<a className='links' href="/src/pages/registrar.jsx">Resgistrar</a>
 </div>
 
         <div className='Bem-vindo'>
           <h1>Bem Vindo ao Clark</h1>
-          <img className='Imagem-cachorro-juiz' height="350px" src="/src/assets/images/download (6).jpeg" alt="" />
-          <div className='circleYellow'></div>
-          <div className='circleYellowPequeno'></div>
-          <div className='circleYellowPequeno2'></div>
-          <div className='circleYellowPequeno3'></div>
-          <div className='circleYellowPequeno4'></div>
-          <div className='circleYellowPequeno5'></div>
+<img className='Imagem-cachorro-juiz' height="350px" src="/src/assets/images/download (6).jpeg" alt="" />
+
+<div className='circleYellow'></div>
+<div className='circleYellowPequeno'></div>
+<div className='circleYellowPequeno2'></div>
+<div className='circleYellowPequeno3'></div>
+<div className='circleYellowPequeno4'></div>
+<div className='circleYellowPequeno5'></div>
         </div>
       </div>
-    </div>
+    
 
   )
 }

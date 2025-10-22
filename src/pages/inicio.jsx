361d6e3a642566e0a562cc0f12d0b901cb202c73
Luizhfu <se.luiz.eduardo@gmail.com>
2025-10-22 15:00:21 -0300

    atualização3

--- src/pages/inicio.jsx
+++ src/pages/inicio.jsx
@@ -7,15 +7,19 @@ export default function Inicio() {
 <div className='Clark'>
 
 <div className='entrada'>
+  <div className='lg'>
 <img className='logo2' src="/src/assets/images/Captura_de_tela_2025-10-14_214454-removebg-preview.png" alt="" />
+  </div>
+<div className='links'>
+<a href="../pages/entrar.jsx">Entrar</a>
+<a href="../pages/registrar.jsx">Resgistrar</a>
+</div>
 
-<a className='links' href="/src/pages/entrar.jsx">Entrar</a>
-<a className='links' href="/src/pages/registrar.jsx">Resgistrar</a>
 </div>
 
         <div className='Bem-vindo'>
           <h1>Bem Vindo ao Clark</h1>
-<img className='Imagem-cachorro-juiz' height="350px" src="/src/assets/images/download (6).jpeg" alt="" />
+<img className='Imagem-cachorro-juiz' height="450px" src="/src/assets/images/download (6).jpeg" alt="" />
 
 <div className='circleYellow'></div>
 <div className='circleYellowPequeno'></div>
